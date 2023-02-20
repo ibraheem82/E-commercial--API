c23441cc3a898f5dbc2167139bc5f2924040d3d8
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-20 23:18:57 +0100

    dotenv/config' was used and used with process.env.

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL
+
+
+
+// http://localhost:3000/api/v1/products
+app.get('/', (req, res) => {
+  res.send('<h1>Omikunle</h1>');
+
+})
 
 app.listen(3000, () => {
   console.log('listening on port http://localhost:3000');
