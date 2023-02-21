076c27d709cbb715dd8854b4454f3e9b0b467303
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-21 10:32:14 +0100

    all the database error was fixed, and the database was successfully connected.

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-require('dotenv/config');
+dotenv.config({ path: './config.env' });
 
-const api = process.env.API_URL
-
-
+const api = process.env.API_URL;
+// * Middlewares
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 // http://localhost:3000/api/v1/products
-app.get('/', (req, res) => {
-  res.send('<h1>Omikunle</h1>');
-
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 90,
+    name: 'Toyota',
+    images: 'homees'
+  }
+  res.send(product);
 })
+
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
+})
+
+
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+).then(() => console.log('DB connection was successfull'));
 
 app.listen(3000, () => {
   console.log('listening on port http://localhost:3000');
