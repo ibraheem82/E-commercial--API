fc943a95bab71d1ae5ed8698a0f1494d2c415f34
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-26 10:15:58 +0100

    logic for the categories router was implemented and tested.

--- routers/categories.router.js
+++ routers/categories.router.js
@@ -26,4 +26,27 @@ router.post('/', async (req, res) => {
     res.send(category)
 })
 
+router.delete('/:id', (req, res) => {
+    // * will find and delete by ID
+    const findID = req.params.id
+    Category.findByIdAndRemove(findID).then(category => {
+        if (category) {
+            return res.status(200).json({
+                success: true,
+                message: `The category with the ID ~ ${findID} ~ was deleted.`
+            })
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: `Category with the ID ~ ${findID} ~ not found.`
+            });
+        }
+    }).catch(err => {
+        return res.status(400).json({
+            success: false,
+            error: err
+        })
+    })
+})
+
 module.exports = router;
