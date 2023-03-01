c0e0897be117c4d589b184bab8c5b7b0a44cd44b
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-01 05:42:54 +0100

    the user post was created to save the user and all the fields we need was created.

--- routers/users.router.js
+++ routers/users.router.js
@@ -11,4 +11,26 @@ router.get(`/`, async (req, res) =>{
     res.send(userList);
 })
 
+router.post('/', async (req, res) => { 
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+//
+    user = await user.save();
+
+    if (!user)
+        return res.status(404).send('The user cannot be created!')
+// * 
+    res.send(user)
+})
+
 module.exports =router;
