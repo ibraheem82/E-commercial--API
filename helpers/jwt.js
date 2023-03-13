aa90d9058ce7ea52c5e764bc51aa0a1d5438da02
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-13 01:14:32 +0100

    Commit message: Excluded certain routes from authentication middleware
    Changes Made:
    
    Used the 'unless()' method provided by the 'express-jwt' library to exclude certain routes from the authentication middleware.
    Specified the paths that do not require authentication in the 'path' property of the configuration object.
    The first path object specifies that the '/api/v1/products' route with 'GET' and 'OPTIONS' methods does not require authentication.
    The second and third paths specify that the '/api/v1/users/login' and '/api/v1/users/register' routes do not require authentication.

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,6 +5,14 @@ function authJwt() {
   return expressJwt({
     secret, 
     algorithms: ['HS256']
+  }).unless({
+    path: [
+      { url: '/api/v1/products', method: ['GET', 'OPTIONS'] },
+      
+      '/api/v1/users/login',
+      '/api/v1/users/register',
+
+    ]
   })
 }
 module.exports = authJwt;
