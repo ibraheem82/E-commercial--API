9764df9099a70e1a57333b95bac51c30e5720c5f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-13 01:15:45 +0100

    Commit message: Set the API URL as an environment variable
    Changes Made:
    
    Created a constant named 'api' that stores the value of the environment variable 'API_URL'.
    The 'API_URL' environment variable can be used to store the base URL of the API server that the application will communicate with.
    Storing the API URL in an environment variable allows for easy configuration and deployment of the application to different environments.

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,6 +2,7 @@ const expressJwt = require('express-jwt');
 
 function authJwt() {
   const secret = process.env.secret;
+  const api = process.env.API_URL;
   return expressJwt({
     secret, 
     algorithms: ['HS256']
