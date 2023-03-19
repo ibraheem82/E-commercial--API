d892beb338c579be931830a9cf1b74c78e010855
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-03-19 17:16:12 +0100

    added some comment to the code...

--- models/order-item.model.js
+++ models/order-item.model.js
@@ -5,6 +5,10 @@ const orderItemSchema = mongoose.Schema({
         type: Number,
         required: true
     },
+    /*
+    * type: mongoose.Schema.Types.ObjectId: This line specifies that the "product" field should store a MongoDB ObjectId, which is a unique identifier used to reference documents in a collection. In other words, the "product" field will store the _id of a related document in another collection.
+
+    */
     product: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Product'
