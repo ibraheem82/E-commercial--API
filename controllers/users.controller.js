1c668d61e6a31f4bb0ed2769f8f6330ef9fa5975
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-04-19 10:48:38 +0100

    feat: Add getAllUsers function to retrieve all users from User model
    The getAllUsers function retrieves all users from the User model, excluding the passwordHash field. It then sends the list of users as a response to the client. The function also checks if the list of users exists, and if not, returns a 500 error response.

---
+++ controllers/users.controller.js
@@ -0,0 +1,16 @@
+const {User} = require('../models/user.model');
+
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    // const userList = await User.find();
+    // ! Excluding the -> [passwordHash] field.
+    const userList = await User.find().select('-passwordHash');
+
+    // Selecting a specific fields you want to use.
+    // const userList = await User.find().select('name email phone');
+
+    if(!userList) {
+        res.status(500).json({success: false})
+    }
+    res.send(userList);
+})
