4138ff9c0cf4df9da9e0bc7c2d8c9c8b9502e282
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-02-27 16:37:52 +0100

    added more things to the files....

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -1,6 +1,7 @@
 // * will only be executed when their is error in our api.
 function errorHandler(err, req, res, next){
  if (err.name === 'UnauthorizedError') {
+  console.log(err)
     return res.status(401).json({message: 'The user is not authorised'})
   }
 
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,6 +9,7 @@ function authJwt() {
     isRevoked: isRevoked
   }).unless({
     path: [
+      // You can access these endpoints without authorization, or the use of JWT , you dont need jwt
       { url: /\/api\/v1\/products(.*)/ , method: ['GET', 'OPTIONS'] },
       {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
       `${api}/users/login`,
@@ -20,10 +21,17 @@ function authJwt() {
 
 
 async function isRevoked(req, payload, done) {
-  if(!payload.isAdmin) {
-        done(null, true)
-    }
-
-    done();
+  // If the user is not an admin, check if the token has been revoked.
+  // For example, you might check a database or cache to see if the token is listed as revoked.
+  // If the token is revoked, call done with true.
+  // If the token is not revoked, call done with false.
+  // If the user is an admin, call done with false to allow access.
+  if (!payload.isAdmin) {
+    // Replace this with your actual logic to check if the token is revoked.
+    const isTokenRevoked = false; // This should be determined based on your application's logic.
+    done(null, isTokenRevoked);
+  } else {
+    done(); // No need to check for revocation, allow access.
+  }
 }
 module.exports = authJwt;
